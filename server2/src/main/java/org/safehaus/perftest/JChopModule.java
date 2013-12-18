56092179b6a1827424337f516212ab220a6ef909
Alex Karasulu <akarasulu@apache.org>
2013-12-19 00:32:19 +0200

    creating new server project for new JUnit Jukito changes

---
+++ server2/src/main/java/org/safehaus/perftest/JChopModule.java
@@ -0,0 +1,55 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: akarasulu
+ * Date: 11/22/13
+ * Time: 11:44 PM
+ */
+package org.safehaus.perftest;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.safehaus.perftest.api.PerftestApiModule;
+import org.safehaus.perftest.client.PerftestClientModule;
+import org.safehaus.perftest.server.rest.ResetResource;
+import org.safehaus.perftest.server.rest.StartResource;
+import org.safehaus.perftest.server.rest.StatsResource;
+import org.safehaus.perftest.server.rest.StatusResource;
+import org.safehaus.perftest.server.rest.StopResource;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.google.inject.servlet.ServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+
+
+public class JChopModule extends ServletModule {
+    public static final String PACKAGES_KEY = "com.sun.jersey.config.property.packages";
+
+
+    protected void configureServlets() {
+        install( new PerftestApiModule() );
+        install( new PerftestClientModule() );
+
+        // Hook Jersey into Guice Servlet
+        bind( GuiceContainer.class );
+
+        // Hook Jackson into Jersey as the POJO <-> JSON mapper
+        bind( JacksonJsonProvider.class ).asEagerSingleton();
+
+        bind( ResultsLog.class ).to( ResultsLogImpl.class );
+
+        bind( CallStats.class );
+        bind( JChopRunner.class ).to( LegacyRunner.class );
+        bind( TestModuleLoader.class );
+        bind( ResetResource.class ).asEagerSingleton();
+        bind( StopResource.class ).asEagerSingleton();
+        bind( StartResource.class ).asEagerSingleton();
+        bind( StatsResource.class ).asEagerSingleton();
+        bind( StatusResource.class ).asEagerSingleton();
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put( PACKAGES_KEY, getClass().getPackage().toString() );
+        serve( "/*" ).with( GuiceContainer.class, params );
+    }
+}
---
+++ server2/src/main/java/org/safehaus/perftest/server/settings/ConfigKeys.java
@@ -0,0 +1,29 @@
+package org.safehaus.perftest.server.settings;
+
+
+/**
+ *
+ */
+public interface ConfigKeys extends org.safehaus.perftest.api.settings.ConfigKeys {
+    String SERVER_PORT_KEY = "server.port";
+    int DEFAULT_SERVER_PORT = 8080;
+
+    String SERVER_INFO_KEY = "server.info";
+    String CONTEXT_PATH = "context.path";
+
+    String CONTEXT_TEMPDIR_KEY = "javax.servlet.context.tempdir";
+    String MANAGER_ENDPOINT_KEY = "manager.endpoint";
+    String DEFAULT_MANAGER_ENDPOINT = "http://localhost:8080/manager/text";
+    String MANAGER_APP_PASSWORD_KEY = "manager.app.password";
+    String MANAGER_APP_USERNAME_KEY = "manager.app.username";
+
+    /** prop key for number of times to retry recovery operations */
+    String RECOVERY_RETRY_COUNT_KEY = "recovery.retry.count";
+    /** default for number of times to retry recovery operations */
+    int DEFAULT_RECOVERY_RETRY_COUNT = 3;
+
+    /** prop key for the time to wait between retry recovery operations */
+    String DELAY_RETRY_KEY = "recovery.retry.delay";
+    /** default for the time to wait in milliseconds between retry recovery operations */
+    long DEFAULT_DELAY_RETRY = 10000;
+}
