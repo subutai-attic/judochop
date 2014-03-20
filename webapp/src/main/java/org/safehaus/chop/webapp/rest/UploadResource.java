3e224c93a888c68aa14dda5adb4fecf5a2122283
bahadyr <bahadyr@acer>
2014-03-20 13:36:46 +0600

    File upload via REST fixed.

--- webapp/src/main/java/org/safehaus/chop/webapp/rest/UploadResource.java
+++ webapp/src/main/java/org/safehaus/chop/webapp/rest/UploadResource.java
@@ -19,89 +19,79 @@
  */
 package org.safehaus.chop.webapp.rest;
 
-
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
+import java.util.logging.Level;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMultipart;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.inject.Inject;
-import com.google.inject.Singleton;
-import com.sun.jersey.multipart.FormDataParam;
-
 
 /**
  * REST operation to upload (a.k.a. deploy) a project war file.
  */
 @Singleton
-@Produces( MediaType.TEXT_PLAIN )
-@Path( UploadResource.ENDPOINT_URL )
+@Produces(MediaType.TEXT_PLAIN)
+@Path(UploadResource.ENDPOINT_URL)
 public class UploadResource {
+
     public final static String ENDPOINT_URL = "/upload";
-    private final static Logger LOG = LoggerFactory.getLogger( UploadResource.class );
+    private final static Logger LOG = LoggerFactory.getLogger(UploadResource.class);
     public static final String FILENAME_PARAM = "file";
     public static final String CONTENT = "content";
-
 
     @Inject
     RestFig config;
 
     @POST
-    @Consumes( MediaType.MULTIPART_FORM_DATA )
-    public Response upload()
-//            @FormDataParam( CONTENT ) InputStream in,
-//            @FormDataParam( FILENAME_PARAM ) String fileName )
-    {
-        LOG.warn( "upload called ..." );
-//        LOG.info( "fileDetails = " + fileName );
-//
-//        // handle the upload of the war file to some path on the file system
-//        String fileLocation = /* config.getWarUploadPath() + */ fileName;
-//        writeToFile( in, fileLocation );
-//
-        return Response.status( Response.Status.CREATED ).entity( "foo" ).build();
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response upload(MimeMultipart multipart) {
+        try {
+            String filename = multipart.getBodyPart(0).getContent().toString();
+            InputStream in = multipart.getBodyPart(1).getInputStream();
+            String fileLocation = /* config.getWarUploadPath() + */ filename;
+            writeToFile(in, fileLocation);
+            return Response.status(Response.Status.CREATED).entity("foo").build();
+        } catch (IOException ex) {
+            LOG.error(ex.getMessage());
+        } catch (MessagingException ex) {
+            LOG.error(ex.getMessage());
+        }
+        return Response.status(Response.Status.CREATED).entity("foo").build();
+
     }
 
-
-    private void writeToFile( InputStream in, String fileLocation )
-    {
+    private void writeToFile(InputStream in, String fileLocation) {
         OutputStream out = null;
 
-        try
-        {
+        try {
             int read;
             byte[] bytes = new byte[1024];
 
-            out = new FileOutputStream( fileLocation );
+            out = new FileOutputStream(fileLocation);
 
-            while ( ( read = in.read( bytes ) ) != -1 )
-            {
-                out.write( bytes, 0, read );
+            while ((read = in.read(bytes)) != -1) {
+                out.write(bytes, 0, read);
             }
             out.flush();
-        }
-        catch ( IOException e )
-        {
-            LOG.error( "Failed to write out file: " + fileLocation, e );
-        }
-        finally
-        {
-            if ( out != null ) {
+        } catch (IOException e) {
+            LOG.error("Failed to write out file: " + fileLocation, e);
+        } finally {
+            if (out != null) {
                 try {
                     out.close();
-                }
-                catch ( IOException e ) {
-                    LOG.error( "Failed while trying to close output stream for {}", fileLocation );
+                } catch (IOException e) {
+                    LOG.error("Failed while trying to close output stream for {}", fileLocation);
                 }
             }
         }
